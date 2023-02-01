c61c7cdd6cd07593c09739f9d395e0e1864fdc02
kchain-solutions <valerio.mellini87@icloud.com>
2023-02-01 13:25:33 +0000

    first commit

---
+++ src/blog-factory.ts
@@ -0,0 +1,34 @@
+import { NewBlog as NewBlogEvent } from "../generated/BlogFactory/BlogFactory"
+import { NewPost as NewPostEvent } from "../generated/templates/Blog/Blog"
+import { NewBlog, NewPost } from "../generated/schema"
+import { Blog } from "../generated/Templates"
+
+export function handleNewBlog(event: NewBlogEvent): void {
+  let entity = new NewBlog(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  Blog.create(event.params.blogAddr);
+
+  entity.owner = event.params.owner
+  entity.blogAddr = event.params.blogAddr
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNewPost(event: NewPostEvent): void {
+  let entity = new NewPost(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner;
+  entity.postAddr = event.params.owner;
+  entity.URI = event.params.URI;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
